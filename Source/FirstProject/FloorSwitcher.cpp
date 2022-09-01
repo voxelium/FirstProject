fa4667e6c1612d5d6dbc732933c69b0be1eb1d03
cgdim <52653488+voxelium@users.noreply.github.com>
2022-09-01 10:24:56 +0300

    Floor switcher update

--- Source/FirstProject/FloorSwitcher.cpp
+++ Source/FirstProject/FloorSwitcher.cpp
@@ -3,11 +3,32 @@
 
 #include "FloorSwitcher.h"
 
+#include "Components/BoxComponent.h"
+
 // Sets default values
 AFloorSwitcher::AFloorSwitcher()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	TriggerBox = CreateDefaultSubobject<UBoxComponent>("Trigger Box");
+
+	// Делаем TriggerBox корневым компонентом
+	RootComponent = TriggerBox;
+	// Указываем тип коллизий - только пересечения. Без взаимодействий с физикой
+	TriggerBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	// Тип коллизий для Trigger Box
+	TriggerBox->SetCollisionObjectType(ECollisionChannel::ECC_WorldStatic);
+	// Запрещаем реагировать на ВСЕ типы коллизий, чтобы потом указать на какой канал нужно реагировать
+	TriggerBox->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	// Указывваем на какой канал коллизий нужно реагировать и как
+	TriggerBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+
+	// Создаем Mesh для Door
+	Door = CreateDefaultSubobject<UStaticMeshComponent>("Door");
+	// Создаем Mesh для Floor Switcher
+	FloorSwitcher = CreateDefaultSubobject<UStaticMeshComponent>("Floor Switcher");
+	FloorSwitcher->SetupAttachment(GetRootComponent());
 
 }
 
@@ -15,6 +36,13 @@ AFloorSwitcher::AFloorSwitcher()
 void AFloorSwitcher::BeginPlay()
 {
 	Super::BeginPlay();
+
+	//указываем какая функция запустится при начале пересечения с TriggerBox
+	TriggerBox->OnComponentBeginOverlap.AddDynamic(this, &AFloorSwitcher::OnOverlapBegin);
+
+	//указываем какая функция запустится по окончанию пересечения с TriggerBox
+	TriggerBox->OnComponentEndOverlap.AddDynamic(this, &AFloorSwitcher::OnOverlapEnd);
+	
 	
 }
 
@@ -24,4 +52,17 @@ void AFloorSwitcher::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
+
+void AFloorSwitcher::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Overlap Begin"));
+}
+
+void AFloorSwitcher::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Overlap END"));
+}
+
 
--- Source/FirstProject/FloorSwitcher.h
+++ Source/FirstProject/FloorSwitcher.h
@@ -15,6 +15,16 @@ public:
 	// Sets default values for this actor's properties
 	AFloorSwitcher();
 
+	// Объем для пересечения, запускающий какой-то функционал. Невидимый
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Floor Switcher")
+	class UBoxComponent* TriggerBox;
+	// Mesh - Отображение trigger box в мире
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Floor Switcher")
+	UStaticMeshComponent* FloorSwitcher;
+	// Mesh - срабатывающий после срабатывания Trigger Box
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Floor Switcher")
+	UStaticMeshComponent* Door;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -23,4 +33,10 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	// функция запустится при начале пересечения
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	// функция запустится по окончанию пересечения
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 };

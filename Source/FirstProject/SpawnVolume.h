4524a9246ecba0b6f0de4e10cb96f1cfd4f75abd
cgdim <52653488+voxelium@users.noreply.github.com>
2022-09-03 13:08:07 +0300

    Spawn Volume start

---
+++ Source/FirstProject/SpawnVolume.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SpawnVolume.generated.h"
+
+UCLASS()
+class FIRSTPROJECT_API ASpawnVolume : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ASpawnVolume();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};

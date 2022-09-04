8b4f57bfef0336451e00f794936ef605f905ba78
cgdim <52653488+voxelium@users.noreply.github.com>
2022-09-04 17:09:32 +0300

    000

--- Source/FirstProject/SpawnVolume.cpp
+++ Source/FirstProject/SpawnVolume.cpp
@@ -2,6 +2,9 @@
 
 
 #include "SpawnVolume.h"
+#include "Components/BoxComponent.h"
+#include "Critter.h"
+#include "Kismet/KismetMathLibrary.h"
 
 
 // Sets default values
@@ -9,6 +12,10 @@ ASpawnVolume::ASpawnVolume()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+	SpawningBox = CreateDefaultSubobject<UBoxComponent>("Spawning Box");
+		 
+		
 }
 
 // Called when the game starts or when spawned
@@ -18,9 +25,38 @@ void ASpawnVolume::BeginPlay()
 	
 }
 
+//-------------------------------FUNCS ---------------
 // Called every frame
 void ASpawnVolume::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 }
+
+//получает точку в пространстве Spawn Volume
+FVector ASpawnVolume::GetSpawnPoint()
+{
+	//получает размер нашего Volume
+	FVector Extent = SpawningBox->GetScaledBoxExtent();
+	//получает центр объекта Volume
+	FVector Origin = SpawningBox->GetComponentLocation();
+	//получает случайную точку в пространстве Spawn Volume
+	FVector Point = UKismetMathLibrary::RandomPointInBoundingBox(Origin, Extent);
+	return Point;
+}
+
+void ASpawnVolume::SpawnOurPawn_Implementation(UClass* ToSpawn, const FVector& Location)
+{
+	if (ToSpawn)
+	{
+		UWorld* World = GetWorld();
+		FActorSpawnParameters SpawnParams;	
+		
+		if (World)
+		{
+			ACritter* ActorSpawned = World->SpawnActor<ACritter>(ToSpawn, Location, FRotator(0.f), SpawnParams); 
+		}
+	}
+}
+
+
 
--- Source/FirstProject/SpawnVolume.h
+++ Source/FirstProject/SpawnVolume.h
@@ -15,11 +15,36 @@ public:
 	// Sets default values for this actor's properties
 	ASpawnVolume();
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Spawn")
+	class UBoxComponent* SpawningBox;
+
+	//Объявляется класс для спавна в Swan Volume
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Spawn")
+	TSubclassOf<APawn>ActorToSpawn;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	// ---------------- FUNCS --------------
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION (BlueprintPure, Category = "Spawn")
+	FVector GetSpawnPoint();
+
+	//Вариант спавна Actor через C++
+	UFUNCTION (BlueprintNativeEvent, BlueprintCallable, Category = "Spawn")
+	void SpawnOurPawn(UClass* ToSpawn, const FVector& Location);
+	
 };
+
+	
+
+
+
+
+
+
+
